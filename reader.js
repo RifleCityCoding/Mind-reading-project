3ec824754b274a1fba259050e0334a9233e3761f
James Spencer <e.spencer@kyspencers.net>
2023-09-22 13:15:21 +0000

    commiting. adding subtext line in html

--- reader.js
+++ reader.js
@@ -119,7 +119,9 @@ const divText = document.getElementById("div-text");
 const subText = document.getElementById("sub-text");
 const nextBtn = document.getElementById("next-button");
 const resetBtn = document.getElementById("reset-button");
-
+// Adding another variable to that show my button when called in my switch casse
+//Needed to move this down to the switch
+//let showResetBtn = document.getElementById("reset-button").hidden = true;
 // Creating a char code string to pull random symbols from.
 
 // Create our array of symbols
@@ -137,35 +139,51 @@ Putting this inside a function
 function getRandomSymbol() {
     const randomSym = Math.floor(Math.random() * yourSym.length);
     return yourSym[randomSym];
-}
+};
 
-
+resetBtn.addEventListener('click', () => {
+    divText.innerHTML = "I can read your mind!";
+    document.getElementById("reset-button").hidden = true;
+});
 // Adding an event listener for when we click the next button.
 
 nextBtn.addEventListener('click', () => {
     // Get the current text of the div
     const currentText = divText.innerHTML;
+    document.getElementById("reset-button").hidden = false;
+
+// Need to add an event listener for reset button.
+
+    
 
     // Call your current text to the page
     switch (currentText) {
         case "I can read your mind!":
             divText.innerHTML = "Pick a number from 1-99";
+            
             break;
         case "Pick a number from 1-99":
             divText.innerHTML = "Add both digits together to get a new number"
             break;
         case "Add both digits together to get a new number":
             divText.innerHTML = "Subtract the new number from your original number"
+            break;
         case "Subtract the new number from your original number":
             divText.innerHTML = 'Scroll down and find your number<br/>';
             //I figured it out! I need to get a symbol assigned to a variable BY itself and THEN assign it to 9
             const commonSymbol = getRandomSymbol();
             for (let i = 0; i <= 99 ; i++) {
                 if (i % 9 === 0){
-                    divText.innerHTML += i + '-' + commonSymbol + '<br>';
+                    divText.innerHTML += i + '=' + commonSymbol + '<br>';
                 }
+                //Annnnnd I broke it because that symbol is also randomly attaching itself to other numbers.
                 else {
                     divText.innerHTML += i + '=' + getRandomSymbol() + '<br>';
                 }
-            }}
-    })
+            }
+            break;
+        case "Scroll down and find your number":
+            divText.innerHTML = "Your symbol is " + commonSymbol + "!<br>";
+            break;
+    }
+})
